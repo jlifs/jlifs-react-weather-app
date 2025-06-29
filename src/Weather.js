c7b56057571efc15f7c3958417cd3e91c4b8282f
jlifs <jlifs74@gmail.com>
2025-06-29 11:25:49 -0400

    Added the api

--- src/Weather.js
+++ src/Weather.js
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
 
 export default function Weather() {
+    const [ready, setReady] = useState(false);
+    const [temperature, setTemperature] = useState(null);
+    function handleResponse(response) {
+        console.log(response.data);
+        setTemperature(response.data.main.temp);
+        setReady(true);
+    }
+
+    if (ready) {
   return (
     <div className="Weather">
       <form>
         <div className="row">
           <div className="col-9">
             <input
-              type="Search"
+              type="search"
               placeholder="Enter a city.."
               className="form-control"
               autoFocus="on"
@@ -16,7 +26,7 @@ export default function Weather() {
           </div>
           <div className="col-3">
             <input
-              type="Submit"
+              type="submit"
               value="Search"
               className="btn btn-primary w-100"
             />
@@ -37,7 +47,7 @@ export default function Weather() {
               className="float-left"
             />
             <div className="float-left">
-              <span className="temperature">6</span>
+              <span className="temperature">{Math.round(temperature)}</span>
               <span className="unit">°C</span>
             </div>
           </div>
@@ -52,4 +62,12 @@ export default function Weather() {
       </div>
     </div>
   );
+} else {
+    const apiKey = "e4a0o883f7063taaf10b8438bb5de43f";
+    let city = "New York";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading...";
+ }
 }
