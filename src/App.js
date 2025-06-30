1503779cea8c88256d674ee8b57c9160ff0bfc25
jlifs <jlifs74@gmail.com>
2025-06-30 17:56:57 -0400

    Added link to footer

--- src/App.js
+++ src/App.js
@@ -7,15 +7,30 @@ export default function App() {
     <div className="App">
       <div className="container">
         <Weather defaultCity="New York" />
-        
+
         <footer>
-          This project was coded by Jennifer Lifsey and is{" "}
+          This project was coded by{" "} 
+          <a href="https://github.com/jlifs"
+          target="_blank"
+          rel="noreferrer">
+          {" "}
+          Jennifer Lifsey{" "} 
+          </a>{" "}
+          and is{" "}
           <a
             href="https://github.com/jlifs/jlifs-react-weather-app"
             target="_blank"
             rel="noreferrer"
           >
-            open-sourced on GitHub
+            open-sourced on GitHub{" "}
+          </a>
+          and{" "}
+          <a
+            href="https://subtle-boba-2cf4a3.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            hosted on Netlify
           </a>
         </footer>
       </div>
